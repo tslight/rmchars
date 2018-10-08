2abe0b077f4b8aafa522d12588f0440351227aa3
tslight <tobyslight@gmail.com>
2018-10-08 18:30:50 +0100

    there is no way to create a node with / in it!

--- create_testdir.py
+++ create_testdir.py
@@ -1,34 +1,62 @@
-# -*- coding: utf-8 -*-
+import argparse
 import os
-# import sys
-# sys.setdefaultencoding('utf-8')
-# reload(sys)
 
-chars = ['\\', '/', '"', ':', '<', '>', '^', '|', '*', '?', '+']
+if os.name == 'nt':
+    chars = ['/', '"', ':', '<', '>', '^', '|', '*', '?', '+']
+else:
+    chars = ['\\', '"', ':', '<', '>', '^', '|', '*', '?', '+']
 
 
-def makenames(name):
+def chkpath(path):
+    """
+    Checks for valid directory path.
+    """
+    if os.path.exists(path):
+        if os.path.isdir(path):
+            return path
+        else:
+            msg = "{0} is not a directory.".format(path)
+    else:
+        msg = "{0} does not exist.".format(path)
+
+    raise argparse.ArgumentTypeError(msg)
+
+
+def getargs():
+    parser = argparse.ArgumentParser(
+        description='Create a bunch of insane files and directories.')
+    parser.add_argument("path", type=chkpath, nargs='?',
+                        default=".", help="A valid path.")
+    return parser.parse_args()
+
+
+def mknames(name):
     names = []
     for i in range(10):
         newname = name + str(i)
         for c in chars:
-            newname = newname + c
+            newname = " " + c + " " + newname + " " + c + " "
+        newname = "   " + newname + "   "
         names.append(newname)
 
     return names
 
 
-def main():
-    dirs = makenames("testdir")
-    files = makenames("testfile")
+def mknodes(path):
+    dirs = mknames("testdir")
+    files = mknames("testfile")
     for d in dirs:
-        # d = d.decode('unicode_escape').encode("utf8")
         if not os.path.exists(d):
-            os.mkdir(d)
+            os.mkdir(os.path.join(path, d))
     for f in files:
-        # f = f.decode('unicode_escape')
         if not os.path.exists(f):
-            open(f, 'a').close()
+            open(os.path.join(path, f), 'a').close()
+
+
+def main():
+    args = getargs()
+    path = os.path.abspath(args.path)
+    mknodes(path)
 
 
 if __name__ == '__main__':
