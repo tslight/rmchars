1589203ae0e483f8d21f4d3ed2a7920566ac8f89
tslight <tobyslight@gmail.com>
2018-10-16 17:16:27 +0100

    finally got it all working and simplified

--- rmchars.py
+++ rmchars.py
@@ -1,6 +1,5 @@
 import argparse
 import os
-import re
 from yorn import ask
 
 chars = ["\\", "/", "\"", ":", "<", ">", "^", "|", "*", "?", "+"]
@@ -21,38 +20,36 @@ def chkpath(path):
     raise argparse.ArgumentTypeError(msg)
 
 
-def replace_chars(path):
+def replace_chars(name):
     """
-    iterate over & delete invalid characters in path.
+    iterate over & delete invalid characters in name.
     """
     for c in chars:
-        path = path.replace(c, "")
-    return path
+        name = name.replace(c, "")
+        name = name.strip()
+    return name
 
 
-def rename_path(root, path):
-    new_name = replace_chars(path)
-    os.rename(os.path.join(root, path), os.path.join(root, new_name))
-
-
-def traverse(path):
-    for root, dirs, files in os.walk(path, topdown=False):
-        for name in dirs:
-            found = re.search(r'[%s]' % ''.join(chars), name)
-            if found:
-                print("Invalid characters found in {0}".format(name))
-                question = "Would you like to rename " + \
-                    name + " to " + replace_chars(name) + " ? "
-                if ask(question):
-                    rename_path(root, name)
-        for name in files:
-            found = re.search(r'[%s]' % ''.join(chars), name)
-            if found:
-                print("Invalid characters found in {0}".format(name))
-                question = "Would you like to rename " + \
-                    name + " to " + replace_chars(name) + " ? "
-                if ask(question):
-                    rename_path(root, name)
+def rename_path(root, name, args):
+    # found = re.search(re.escape("|".join(chars), name))
+    # https://stackoverflow.com/a/5858943
+    # any + generator is more robust
+    found = any(c in name for c in chars)
+    if found:
+        oldpath = os.path.join(root, name)
+        newpath = os.path.join(root, replace_chars(name))
+        if args.interactive:
+            question = "Would you like to rename " + \
+                oldpath + " to " + newpath + " ? "
+            if ask(question):
+                os.rename(oldpath, newpath)
+        elif args.automate:
+            print("RENAMING: {} to {}".format(oldpath, newpath))
+            os.rename(oldpath, newpath)
+        elif args.dry_run:
+            print("TESTING: {} to {}".format(oldpath, newpath))
+        elif args.quiet:
+            os.rename(oldpath, newpath)
 
 
 def getargs():
@@ -61,15 +58,28 @@ def getargs():
     """
     parser = argparse.ArgumentParser(
         description='Remove invalid characters from a given path.')
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument("-i", "--interactive", action="store_true",
+                       help="prompt before renaming each path")
+    group.add_argument("-a", "--automate", action="store_true",
+                       help="rename each path without prompting")
+    group.add_argument("-t", "--dry_run", action="store_true",
+                       help="preform a dry run to see what would be renamed")
+    group.add_argument("-q", "--quiet", action="store_true",
+                       help="run silently")
     parser.add_argument("path", type=chkpath, nargs='?',
-                        default=".", help="A valid path.")
+                        default=".", help="a valid path")
     return parser.parse_args()
 
 
 def main():
     args = getargs()
     path = os.path.abspath(args.path)
-    traverse(path)
+    for root, dirs, files in os.walk(path, topdown=False):
+        for name in files:
+            rename_path(root, name, args)
+        for name in dirs:
+            rename_path(root, name, args)
 
 
 if __name__ == '__main__':
