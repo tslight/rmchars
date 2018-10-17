137f191888b54c5ea0d3e7f465cdcaaf51cc9f09
tslight <tobyslight@gmail.com>
2018-10-17 18:01:25 +0100

    pass args to rmchars

--- run.sh
+++ run.sh
@@ -10,9 +10,16 @@ getelcs () {
 
 getcreds () {
     read -re -p "Enter your Egnyte Admin username: " USER
-    read -res -p "Enter you Egnyte Admin password: " pass
-    PASS=$(printf '%q' "$pass") # escape special bash chars
-    # PASS=${PASS// /\\ } # escape spaces
+    read -res -p "Enter you Egnyte Admin password: " PASS
+    # USER=$(printf '%q' "$user") # escape special bash chars
+    # PASS=$(printf '%q' "$pass") # escape special bash chars
+}
+
+getshare () {
+    local elc="$1" share
+
+    share="//${USER}:${PASS}@${elc}/ELC"
+    SHARE="${share// /%20}"
 }
 
 chkdir () {
@@ -28,37 +35,37 @@ chkdir () {
 }
 
 mntelc () {
-    local elc="$1"
-    local mntpt="$2"
+    local elc="$1" MNT="$2"
 
-    if ! mount | grep -Eq "$elc.*$mntpt"; then
-	if mount -t smbfs "//${USER}:${PASS}@${elc}/ELC/Shared" "$mntpt"; then
-	    echo "Successfully mounted $elc at $mntpt"
+    if ! mount | grep -Eq "$elc.*$MNT"; then
+	getshare "$elc"
+	if mount -t smbfs "$SHARE" "$MNT" &> /dev/null; then
+	    echo "Successfully mounted $elc at $MNT"
 	else
-	    echo "Failed to mount $elc at $mntpt"
+	    echo "Failed to mount $elc at $MNT"
 	fi
     else
-	echo "$elc already mounted at $mntpt"
+	echo "$elc already mounted at $MNT"
     fi
 }
 
 main () {
-    local mntpt
+    local args="$*" MNT
 
     getelcs
     getcreds
 
     for elc in "${ELCS[@]}"; do
-	mntpt="/Volumes/ELCS/$elc"
-	MOUNTS+=('mntpt')
+	MNT="/Volumes/ELCS/$elc"
+	MOUNTS+=("$MNT")
 
-	chkdir "$mntpt"
-	mntelc "$elc" "$mntpt"
+	chkdir "$MNT"
+	mntelc "$elc" "$MNT"
     done
 
     for mnt in "${MOUNTS[@]}"; do
-	rmchars -f "$mnt"
+	rmchars "$args" "$mnt"
     done
 }
 
-main
+main "$@"
