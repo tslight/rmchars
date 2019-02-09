954a028a4286e8cbac1dcfe3a5ecc6586ce2caa1
tslight <tobyslight@gmail.com>
2019-02-09 20:22:04 +0000

    fix linting complaints

--- rmchars/rmchars.py
+++ rmchars/rmchars.py
@@ -1,57 +1,55 @@
 # Copyright (c) 2018, Toby Slight. All rights reserved.
 # ISC License (ISCL) - see LICENSE file for details.
-
+"""
+Module that carries out the heavy lifting.
+"""
 import os
-import sys
 import unicodedata
 from yorn import ask
 
-chars = ["\\", "/", "\"", ":", "<", ">", "^", "|", "*", "?"]
+CHARS = ["\\", "/", "\"", ":", "<", ">", "^", "|", "*", "?"]
 # http://www.unicode.org/reports/tr44/#GC_Values_Table
-uchars = ["Lo", "So", "Cc"]  # Other_Letter, Other_Symbol, Control_Character
+UCHARS = ["Lo", "So", "Cc"]  # Other_Letter, Other_Symbol, Control_Character
 
 
 def replace_chars(name):
     """
     iterate over & delete invalid characters in name.
     """
-
-    for c in name:
-        if c in chars:
-            name = name.replace(c, "")
-
-        # u = c.encode('unicode_escape')  # get unicode encoding
-        # if len(u) > 4:  # not as simple as it sounds!
-        #     # some unicode characters take up two ascii chars.... what a world..
-        #     next_char_index = name.index(c) + 1
-        #     if next_char_index < len(name):
-        #         name = name.replace(name[next_char_index], "")
-        #     name = name.replace(c, "")
-
+    for char in name:
+        if char in CHARS:
+            name = name.replace(char, "")
         # https://stackoverflow.com/questions/4324790/removing-control-characters-from-a-string-in-python
-        if unicodedata.category(c) in uchars:
-            name = name.replace(c, "")
-
+        if unicodedata.category(char) in UCHARS:
+            name = name.replace(char, "")
         name = name.strip()  # remove leading and trailing spaces
-
         if name.endswith("."):
             name = name[:-1]  # remove last char
-
     return name
 
 
 def is_invalid(name, invalid=False):
-    for c in name:
-        if c in chars or unicodedata.category(c) in uchars:
+    """
+    Checks if any of invalid chars are present in name of the node and returns
+    true if they are.
+    """
+    # found = re.search(re.escape("|".join(chars), name))
+    # https://stackoverflow.com/a/5858943
+    # any + generator is more robust
+    # char_found = any(c in name for c in chars)
+    for char in name:
+        if char in CHARS or unicodedata.category(char) in UCHARS:
             invalid = True
     return invalid
 
 
 def rename_path(root, name, args):
-    # found = re.search(re.escape("|".join(chars), name))
-    # https://stackoverflow.com/a/5858943
-    # any + generator is more robust
-    # char_found = any(c in name for c in chars)
+    """
+    Use is_invalid to check if node name contains invalid chars, and if true use
+    replace_chars to create a new node name and join it to the full path.
+
+    Finally, check args on how to proceed.
+    """
     if is_invalid(name):
         oldpath = os.path.join(root, name)
         newpath = os.path.join(root, replace_chars(name))
