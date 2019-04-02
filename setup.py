88151b1f3ee12a436a93e21b8825acf846bcc4c9
tslight <tobyslight@gmail.com>
2019-04-02 16:56:08 +0100

    lazygitpush

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="rmchars",
-    version="0.0.4",
+    version="0.0.5",
     author="Toby Slight",
     author_email="tobyslight@gmail.com",
     description="Recursively remove invalid characters from path names.",
